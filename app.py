5ea750b2c202ae803c7db90d8fb2ee7f3333f679
Andrea V <33574513+andvg@users.noreply.github.com>
2022-01-29 03:15:25 +0100

    Dev

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from google.oauth2 import service_account
+from gsheetsdb import connect
 
 st.title('MVP-workflow-analysis-st')
+
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.Collaboratore} has a :{row.Ponte}:")
